611527e7a8600c901893eee74f964dcbe96de5d5
flogrv <florian.grave@epitech.eu>
2024-06-24 19:47:56 +0200

    Update shortcut

--- lua/epitech/init.lua
+++ lua/epitech/init.lua
@@ -27,7 +27,7 @@ end
 function M.setup(opts)
     opts = opts or {}
     vim.api.nvim_create_user_command("EpitechHeader", createHeader, {})
-    vim.keymap.set('n', '<leader>h', createHeader, {
+    vim.keymap.set('n', '<leader>he', createHeader, {
         noremap = true,
         desc = "shortcut insert epitech header",
         silent = true
